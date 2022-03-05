f3eda96baf597bcf1c8b9e76b28ea6822a40b353
ahmedboukriba98 <98738608+ahmedboukriba98@users.noreply.github.com>
2022-03-05 22:18:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var name= new Array();
+names[0]="Yaakov";
+names[1]="Jhon";
+names[2]="jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frankkov";
+names[6]="larry";
+names[7]="paul";
+names[8]="laura";
+names[9]="jim";
+
+for (var i=0;i<names.length;i++)
+{
+    if(names[i].charAt() === 'j' || names[i].charAt()==='J')
+    {
+       console.log("good by"+names[i])
+    }
+    else{
+        console.log("hello "+names[i])
+    }
+    
+}
